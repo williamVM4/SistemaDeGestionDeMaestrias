911afdc4790ffe2429b153e7e76f761d931926e3
GusPD <86135999+GusPD@users.noreply.github.com>
2023-05-18 16:09:41 -0600

    actualizacionRama

--- src/main/java/com/gl05/bad/controller/AspiranteProfesorController.java
+++ src/main/java/com/gl05/bad/controller/AspiranteProfesorController.java
@@ -6,9 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AspiranteProfesorController {
-   @GetMapping("/AspiranteProfesor")
+    @GetMapping("/AspiranteProfesor")
     public String perfilAspiranteProfesor(Model model) {
         model.addAttribute("pageTitle", "PerfilAspiranteProfesor");
         return "/AspiranteProfesor/perfilAspiranteProfesor";
     }
+    
+    @GetMapping("/GestionarAspiranteProfesor")
+    public String gestionarAspiranteProfesor(Model model) {
+        model.addAttribute("pageTitle", "GestionarAspiranteProfesor");
+        return "/AspiranteProfesor/gestionarAspiranteProfesor";
+    }
+    
+    /*@PostMapping("/AgregarAspiranteProfesor")
+    public String agregarAspiranteProfesor(AspiranteProfesor aspiranteProfesor){
+        aspiranteProfesorService.agregar(aspiranteProfesor);
+        return "redirect:/GestionarAspiranteProfesor";
+    }*/
 }
