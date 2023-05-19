33dc04780464622c123a5b6af2fab402b41971c7
CarolinaIPD <pd19007@ues.edu.sv>
2023-05-18 22:49:44 -0600

    CAmbios x

--- src/main/java/com/gl05/bad/domain/CoordinadorAcademico.java
+++ src/main/java/com/gl05/bad/domain/CoordinadorAcademico.java
@@ -1,81 +1,344 @@
 package com.gl05.bad.domain;
 
-import java.io.Serializable;
 import java.sql.Blob;
-import java.util.Date;
 import javax.persistence.*;
 import lombok.Data;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-@Data
 @Entity
 @Table(name="COORDINADOR_ACADEMICO")
+@NamedQueries({
+  @NamedQuery(name = "CoordinadorAcademico.findAll", query = "SELECT c FROM CoordinadorAcademico c"),
+  @NamedQuery(name = "CoordinadorAcademico.findByIdCoorAca", query = "SELECT c FROM CoordinadorAcademico c WHERE c.idCoorAca = :idCoorAca"),
+  @NamedQuery(name = "CoordinadorAcademico.findByCodCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.codCa = :codCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByNombresCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.nombresCa = :nombresCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByApellidosCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.apellidosCa = :apellidosCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findBySexoCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.sexoCa = :sexoCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByGeneroCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.generoCa = :generoCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByFechaNacCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.fechaNacCa = :fechaNacCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByEstadoCivilCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.estadoCivilCa = :estadoCivilCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByNacionalidadCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.nacionalidadCa = :nacionalidadCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByDuiCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.duiCa = :duiCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByNitCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.nitCa = :nitCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByNupCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.nupCa = :nupCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByPasaporteCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.pasaporteCa = :pasaporteCa"),
+  @NamedQuery(name = "CoordinadorAcademico.findByDocPersonalCa", query = "SELECT c FROM CoordinadorAcademico c WHERE c.docPersonalCa = :docPersonalCa")})
+
 public class CoordinadorAcademico implements Serializable{
   private static final long serialVersionUID = 1L;
   
-  @Column(name="ID_COOR_ACA")
   @Id
+  @Basic(optional = false)
+  @Column(name="ID_COOR_ACA")
   @SequenceGenerator(name = "S_COORDINADOR_ACADEMICO", sequenceName = "S_COORDINADOR_ACADEMICO", allocationSize = 1)
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "S_COORDINADOR_ACADEMICO")
-    private int idCA;
- 
-  @Column(name="IDUSUARIO", nullable = true)
-    private Integer idUsuario;
+    private Integer idCoorAca;
   
   @Column(name="COD_CA", nullable = true)
-    private String codCA;
+    private String codCa;
   
   @Column(name="NOMBRES_CA", nullable = true)
-    private String nombresCA;
+    private String nombresCa;
   
   @Column(name="APELLIDOS_CA", nullable = true)
-    private String apellidosCA;
+    private String apellidosCa;
   
   @Column(name="SEXO_CA", nullable = true)
-    private String sexoCA;
+    private String sexoCa;
   
   @Column(name="GENERO_CA", nullable = true)
-    private String generoCA;
+    private String generoCa;
   
   @Column(name="FECHA_NAC_CA", nullable = true)
-    private Date fechaNacCA;  
+  @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaNacCa;  
 
   @Column(name="ESTADO_CIVIL_CA", nullable = true)
-    private String estadoCivilCA;
+    private String estadoCivilCa;
   
   @Column(name="NACIONALIDAD_CA", nullable = true)
-    private String nacionalidadCA;
-   
-  @Column(name="ID_PAIS", nullable = true)
-    private Integer idPais;
+    private String nacionalidadCa;
 
+  @Lob
   @Column(name="FOTOGRAFIA_CA", nullable = true)
-    private Blob fotografia;
+    private Blob fotografiaCa;
   
   @Column(name="DUI_CA", nullable = true)
-    private String duiCA;
+    private String duiCa;
   
   @Column(name="NIT_CA", nullable = true)
-    private String nitCA;
+    private String nitCa;
   
   @Column(name="NUP_CA", nullable = true)
-    private String nupCA;
+    private String nupCa;
   
   @Column(name="PASAPORTE_CA", nullable = true)
-    private String pasaporteCA;
+    private String pasaporteCa;
   
   @Column(name="DOC_PERSONAL_CA", nullable = true)
-    private String docPersonalCA;
+    private String docPersonalCa;
+ 
+  @JoinColumn(name = "ID_LIST_CORREO", referencedColumnName = "ID_LIST_CORREO", nullable = true)
+  @ManyToOne(optional = false)
+    private ListadoCorreo idListCorreo;
 
-    @Column(name="ID_LIST_TELEFONO", nullable = true)
-    private Integer idListTelefono;
+  @JoinColumn(name = "ID_LIST_DP", referencedColumnName = "ID_LIST_DP", nullable = true)
+  @ManyToOne
+    private ListadoDocumentacionPersonal idListDp;
+
+  @JoinColumn(name = "ID_LIST_TELEFONO", referencedColumnName = "ID_LIST_TELEFONO", nullable = true)
+  @ManyToOne
+    private ListadoTelefono idListTelefono;
+
+  @JoinColumn(name = "ID_LIST_TA", referencedColumnName = "ID_LIST_TA", nullable = true)
+  @ManyToOne
+    private ListadoTitulosAcademicos idListTa;
+
+  @JoinColumn(name = "ID_PAIS", referencedColumnName = "ID_PAIS", nullable = true)
+  @ManyToOne(optional = false)
+    private Pais idPais;
+
+  @JoinColumn(name = "IDUSUARIO", referencedColumnName = "IDUSUARIO")
+  @ManyToOne
+    private Usuario idusuario;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "idCoorAca")
+    private Collection<Maestria> maestriaCollection;
   
-  @Column(name="ID_LIST_DP", nullable = true)
-    private Integer idListDP;
+  public CoordinadorAcademico() {}
 
-   @Column(name="ID_LIST_TA", nullable = true)
-    private Integer idListTA;
+  public CoordinadorAcademico(Integer idCoorAca) {
+    this.idCoorAca = idCoorAca;
+  }
 
-   @Column(name="ID_LIST_CORREO", nullable = true)
-    private Integer idListCorreo;
+  public CoordinadorAcademico(Integer idCoorAca, String codCa, String nombresCa, String apellidosCa, String sexoCa, String generoCa, Date fechaNacCa, String estadoCivilCa, String nacionalidadCa, Blob fotografiaCa) {
+    this.idCoorAca = idCoorAca;
+    this.codCa = codCa;
+    this.nombresCa = nombresCa;
+    this.apellidosCa = apellidosCa;
+    this.sexoCa = sexoCa;
+    this.generoCa = generoCa;
+    this.fechaNacCa = fechaNacCa;
+    this.estadoCivilCa = estadoCivilCa;
+    this.nacionalidadCa = nacionalidadCa;
+    this.fotografiaCa = fotografiaCa;
+  }
+
+  public int getIdCoorAca() {
+    return idCoorAca;
+  }
+
+  public void setIdCoorAca(int idCoorAca) {
+    this.idCoorAca = idCoorAca;
+  }
+
+  public String getCodCa() {
+    return codCa;
+  }
+
+  public void setCodCa(String codCa) {
+    this.codCa = codCa;
+  }
+
+  public String getNombresCa() {
+    return nombresCa;
+  }
+
+  public void setNombresCa(String nombresCa) {
+    this.nombresCa = nombresCa;
+  }
+
+  public String getApellidosCa() {
+    return apellidosCa;
+  }
+
+  public void setApellidosCa(String apellidosCa) {
+    this.apellidosCa = apellidosCa;
+  }
+
+  public String getSexoCa() {
+    return sexoCa;
+  }
+
+  public void setSexoCa(String sexoCa) {
+    this.sexoCa = sexoCa;
+  }
+
+  public String getGeneroCa() {
+    return generoCa;
+  }
+
+  public void setGeneroCa(String generoCa) {
+    this.generoCa = generoCa;
+  }
+
+  public Date getFechaNacCa() {
+    return fechaNacCa;
+  }
+
+  public void setFechaNacCa(Date fechaNacCa) {
+    this.fechaNacCa = fechaNacCa;
+  }
+
+  public String getEstadoCivilCa() {
+    return estadoCivilCa;
+  }
+
+  public void setEstadoCivilCa(String estadoCivilCa) {
+    this.estadoCivilCa = estadoCivilCa;
+  }
+
+  public String getNacionalidadCa() {
+    return nacionalidadCa;
+  }
+
+  public void setNacionalidadCa(String nacionalidadCa) {
+    this.nacionalidadCa = nacionalidadCa;
+  }
+
+  public Blob getFotografiaCa() {
+    return fotografiaCa;
+  }
+
+  public void setFotografiaCa(Blob fotografiaCa) {
+    this.fotografiaCa = fotografiaCa;
+  }
+
+  public String getDuiCa() {
+    return duiCa;
+  }
+
+  public void setDuiCa(String duiCa) {
+    this.duiCa = duiCa;
+  }
+
+  public String getNitCa() {
+    return nitCa;
+  }
+
+  public void setNitCa(String nitCa) {
+    this.nitCa = nitCa;
+  }
+
+  public String getNupCa() {
+    return nupCa;
+  }
+
+  public void setNupCa(String nupCa) {
+    this.nupCa = nupCa;
+  }
+
+  public String getPasaporteCa() {
+    return pasaporteCa;
+  }
+
+  public void setPasaporteCa(String pasaporteCa) {
+    this.pasaporteCa = pasaporteCa;
+  }
+
+  public String getDocPersonalCa() {
+    return docPersonalCa;
+  }
+
+  public void setDocPersonalCa(String docPersonalCa) {
+    this.docPersonalCa = docPersonalCa;
+  }
+
+  public ListadoCorreo getIdListCorreo() {
+    return idListCorreo;
+  }
+
+  public void setIdListCorreo(ListadoCorreo idListCorreo) {
+    this.idListCorreo = idListCorreo;
+  }
+
+  public ListadoDocumentacionPersonal getIdListDp() {
+    return idListDp;
+  }
+
+  public void setIdListDp(ListadoDocumentacionPersonal idListDp) {
+    this.idListDp = idListDp;
+  }
+
+  public ListadoTelefono getIdListTelefono() {
+    return idListTelefono;
+  }
+
+  public void setIdListTelefono(ListadoTelefono idListTelefono) {
+    this.idListTelefono = idListTelefono;
+  }
+
+  public ListadoTitulosAcademicos getIdListTa() {
+    return idListTa;
+  }
+
+  public void setIdListTa(ListadoTitulosAcademicos idListTa) {
+    this.idListTa = idListTa;
+  }
+
+  public Pais getIdPais() {
+    return idPais;
+  }
+
+  public void setIdPais(Pais idPais) {
+    this.idPais = idPais;
+  }
+
+  public Usuario getIdusuario() {
+    return idusuario;
+  }
+
+  public void setIdusuario(Usuario idusuario) {
+    this.idusuario = idusuario;
+  }
+
+  public Collection<Maestria> getMaestriaCollection() {
+    return maestriaCollection;
+  }
+
+  public void setMaestriaCollection(Collection<Maestria> maestriaCollection) {
+    this.maestriaCollection = maestriaCollection;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (idCoorAca != null ? idCoorAca.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+      if (this == object) {
+          return true;
+      }
+      if (object == null || getClass() != object.getClass()) {
+          return false;
+      }
+      CoordinadorAcademico other = (CoordinadorAcademico) object;
+      return idCoorAca == other.idCoorAca;
+  }
+
+  @Override
+  public String toString() {
+    return "com.gl05.bad.domain.CoordinadorAcademico[ idCoorAca=" + idCoorAca + " ]";
+  }
+
   
 }
--- src/main/webapp/WEB-INF/jsp/coordinadorAcademico/viewCoordinadorAcademico.jsp
+++ src/main/webapp/WEB-INF/jsp/coordinadorAcademico/viewCoordinadorAcademico.jsp
@@ -37,9 +37,9 @@
                       <c:forEach items="${coordinadoresAC}" var="elemento" varStatus="status">
                           <tr>
                               <td width="20%">${status.index + 1}</td>
-                              <td>${elemento.codCA}</td>
-                              <td>${elemento.nombresCA}</td>
-                              <td>${elemento.apellidosCA}</td>
+                              <td>${elemento.codCa}</td>
+                              <td>${elemento.nombresCa}</td>
+                              <td>${elemento.apellidosCa}</td>
                               <td>Ver coordinador</td>
                           </tr>
                       </c:forEach>
@@ -54,46 +54,35 @@
     </div>
   </div>
 </div>
-<!--<div align="center">
-        <div class="titulo-Perfil"><h3>Gestionar Aspirante a Profesor</h3></div>
-        <div id="container-datos">
-            <div class="row col-sm-12 d-flex justify-content-end">
-                <div class="col-sm-1">
-                    <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#crearModal">Agregar</button>
-                </div>
-            </div>
-            <br>
-            
-        </div>
-    </div>
+
     <!-- Modal para crear coordinadores -->
     <div class="modal fade" id="crearModal" tabindex="-1" aria-labelledby="crearModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="crearModalLabel">Agregar Coordinador Académico</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="AspiranteProfesorController" method="post">
-                        <input type="hidden" name="action" value="create">
-                        <div class="form-group">
-                            <label for="nombre">Nombre:</label>
-                            <input type="text" class="form-control" id="nombre" name="nombre" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email:</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-outline-success">Guardar</button>
-                            <button type="submit" class="btn btn-outline-danger" data-bs-dismiss="modal">Cancelar</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="crearModalLabel">Agregar Coordinador Académico</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form action="AspiranteProfesorController" method="post">
+              <input type="hidden" name="action" value="create">
+              <div class="form-group">
+                <label for="nombre">Nombre:</label>
+                <input type="text" class="form-control" id="nombre" name="nombre" required>
+              </div>
+              <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-outline-success">Guardar</button>
+                <button type="submit" class="btn btn-outline-danger" data-bs-dismiss="modal">Cancelar</button>
+              </div>
+            </form>
+          </div>
         </div>
-    </div>-->
+      </div>
+    </div>
 <%@ include file="../common/footer.jspf"%>
 
 
