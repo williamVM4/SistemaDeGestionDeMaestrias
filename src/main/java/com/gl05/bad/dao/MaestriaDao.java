22a0e77ca0ef63c003b5eab68ebd189c480a1c68
williamVM4 <enriquemancia25@gmail.com>
2023-05-22 19:11:10 -0600

    datatable serverside maestria

--- src/main/java/com/gl05/bad/dao/MaestriaDao.java
+++ src/main/java/com/gl05/bad/dao/MaestriaDao.java
@@ -2,11 +2,11 @@ package com.gl05.bad.dao;
 
 import com.gl05.bad.domain.Maestria;
 import java.util.Collection;
+import org.springframework.data.jpa.datatables.repository.DataTablesRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface MaestriaDao extends JpaRepository<Maestria, Integer>{
-    @Query("SELECT m FROM Maestria m WHERE m.nombreMaestria LIKE %:searchValue%")
-    Collection<Maestria> findBySearchValue(@Param("searchValue") String searchValue);
+public interface MaestriaDao extends DataTablesRepository<Maestria, Long>{
+
 }
